0fae657cd9c70759db45e3769ba8ca7f8e2946fd
ahmadarsim <ahmadarsim2018@gmail.com>
2021-03-01 13:10:37 +0500

    few lines added

--- testing.r
+++ testing.r
@@ -1,3 +1,9 @@
-hellow R world
+# hellow R world
 print("hellow R world")
 
+print(Rfamily)
+print("how are you people R family")
+
+introduction = "who Am I"
+print("I am upcoming kaggle master")
+ 
